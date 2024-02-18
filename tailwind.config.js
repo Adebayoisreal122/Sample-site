a993d7a1a0ee73ea967a0f19bfa16b7a65368e81
adebayo isreal <adebayoisrealight122@gmail.com>
2024-02-18 21:22:44 +0100

    i made changes on the tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    // "./index.html",
-    // "./src/**/*.{js,ts,jsx,tsx}",
-    // "./node_modules/tw-elements/dist/js/**/*.js",
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/tw-elements/dist/js/**/*.js",
+    './components/Wellcome',
+    '.src/App.jsx'
   ],
   theme: {
     extend: {},
@@ -12,7 +14,10 @@ export default {
   darkMode: 'class',
   // ...
 
-  plugins: [],
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+  ],
 }
 
 
