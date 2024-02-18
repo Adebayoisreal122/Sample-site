248f83d45b44fa96c00d4bc61dc579a7af1ee6b5
adebayo isreal <adebayoisrealight122@gmail.com>
2024-02-18 21:06:20 +0100

    i added some files

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import { useState } from 'react'
 import { Routes,Route, Navigate } from 'react-router-dom'
 import './App.css';
-
-
 import Wellcome from './components/Wellcome';
+import AllPage from './components/AllPage';
+
+
+
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -12,7 +15,11 @@ function App() {
     <>   
    <Routes>
   
+  <Route path="/" element={<AllPage/>} >
   <Route path="/" element={<Wellcome/>} />
+  
+  </Route>
+
 
 </Routes>
 
